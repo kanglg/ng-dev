e0c621eaeb84017420ea5cf4b755532743287d8c
kanglg <kanglgt@163.com>
2016-07-19 23:55:14 +0800

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngDevApp
+ * @description
+ * # ngDevApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngDevApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngDevApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngDevApp
+ */
+angular.module('ngDevApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
